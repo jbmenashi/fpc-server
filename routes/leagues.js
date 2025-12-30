5b51901593c5b15cd46c43a4e0b42ace504b4581
Jacob Menashi <jbmenashi@gmail.com>
2025-12-29 22:53:00 -0600

    drafted field fix

--- routes/leagues.js
+++ routes/leagues.js
@@ -52,7 +52,7 @@ router.post("/", requireClerkAuth, async (req, res) => {
 
 router.put("/:id", requireClerkAuth, async (req, res) => {
   const { id } = req.params;
-  const { leagueName, size, full } = req.body || {};
+  const { leagueName, size, full, drafted } = req.body || {};
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(400).json({ error: "Invalid league ID" });
